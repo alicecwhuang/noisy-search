7ad01f744cf418f1b19d11f674080a9d4d017491
Alice Huang <alicealice.huang@mail.utoronto.ca>
2023-04-24 10:22:58 -0400

    Bugs fixed, added parallelization and replicated Grim et al.

--- model.py
+++ model.py
@@ -17,57 +17,42 @@ import concurrent.futures
 
 
 class Landscape:
-    def __init__(self, smoothness, length=500):
+    def __init__(self, smoothness, length=2000):
 
         self.s = smoothness
         self.length = length
-        
+
+        """Generate landscape heights"""
         total_segments = round(self.length/self.s)
-        """Choose set of random points"""
-        points = random.choices(range(1, 100), k=total_segments)
+        points = [random.choice(range(1, 101))] 
         heights = []
-        for i in range(total_segments):
-            """Smoothen between every two points"""
-            a = points[i]
-            b = points[(i+1)%total_segments]
-            step = np.round((b - a)/self.s, 2)
-            for j in range(self.s):
-                if a + step*j > 100:
+        
+        for j in range(total_segments-1):
+            a = points[-1]
+            b = random.choice(range(1, 101))
+            points.append(b) # random points to define landscape
+            seg_len = random.choice(range(1, 2*self.s))
+            """Fill in locations between two points"""
+            step = np.round((b - a)/seg_len, 2)
+            for i in range(1, seg_len+1):
+                if a + step*i > 100:
                     heights.append(100)
                 else:
-                    heights.append(a + step*j)
+                    heights.append(a + step*i)
+            
         self.heights = heights
-        """Update in case original length not divisible by smoothness"""
         self.length = len(self.heights)
-
-
-# In[8]:
-
-
-def cov_sim(set1, set2):
-    """count how many step sizes overlap between agents"""
-    count = 0
-    for i in set1:
-        if i in set2:
-            count += 1
-    return count
-
-
-# ### Model
-
-# In[235]:
-
 
 class Agent():
     def __init__(self, no, h, landscape, sigma=0):
         self.no = no
         self.h = h # Ordered list of step sizes
         self.landscape = landscape
-        self.default_start = random.choice(range(landscape.length))
         self.score = 0 # Highest value found
         self.sigma = sigma # Level of noise in signals
         
     def data(self, loc):
+        """Get noisy data from location"""
         return max(np.random.normal(self.landscape.heights[loc], 
                                     self.sigma), 
                    0.001)
@@ -93,7 +78,7 @@ class Agent():
             nxt = (loc + self.h[n_total%3])%self.landscape.length # Next loc to check
             if in_hist[nxt] == 0:
                 """Never checked by ingroup"""
-                value = self.data(loc) # Noisy
+                value = self.data(nxt)
                 findings[nxt], in_hist[nxt] = value, value
                 if maxi < value:
                     """Found value higher than current one"""
@@ -113,11 +98,12 @@ class Agent():
                     n_total += 1
         return findings
 
+    
 class Team():
     def __init__(self, members, landscape, trust_level=1):
         self.members = members
         self.landscape = landscape
-        self.trust_level = trust_level
+        self.trust_level = trust_level # Size of trusted subgroups
         self.trust = dict()
         for a in self.members:
             self.trust[a] = [a]
@@ -132,6 +118,7 @@ class Team():
                     self.trust[b] = subgroup
     
     def aggregate(self, maps):
+        """Aggregate search results from multiple agents"""
         denom = np.sum([np.where(m>0, 1, 0) for m in maps.values()], axis=0)
         num = np.sum([m for m in maps.values()], axis=0)
         return num/(denom + np.where(denom==0, 1,0))
@@ -157,109 +144,96 @@ class Team():
 
         return self.landscape.heights[np.argmax(self.aggregate(maps))]
 
-# In[248]:
-
-
-class CorrTeam(Team):
-    def __init__(self, members, landscape, trust_level=0.5):
-        super().__init__(members, landscape, trust_level)
-        self.trust = dict()
-        for a in self.members:
-            self.trust[a] = [a]
-        if self.trust_level > 0:
-            M = self.members
-            k = math.ceil(len(self.members)*self.trust_level)
-            while len(M) > 0:
-                rmd = random.choice(M)
-                M.sort(key=lambda x: cov_sim(rmd.h, x.h), reverse=True)
-                subgroup = M[:k]
-                for b in subgroup:
-                    self.trust[b] = subgroup
-                M = M[k:]
 
 
 # In[307]:
 
-Poolsizes = [12]
+poolsize = 12
+pool = list(range(1, poolsize+1))
+h_each = 3 # Number of step sizes each agent has
+per_team = 9 # Number of agents per team
+all_perm = list(itertools.permutations(pool, r=h_each)) # All possible step size combo
 Smoothness = range(1, 9)
-Sigma = [0]
+sigma = 0
 Trust = [0, 0.33, 0.5, 1]
+runs = 1
 
+LS = [] # List of landscapes to test
+Experts = dict()
+for s in Smoothness:
+    for run in range(runs):
+        np.random.seed()
+        L = Landscape(s)
+        LS.append(L)
+
+def calc_score(a, L):
+    """Calculate agent's avg search score from all starting points of landscape"""
+    scores = [] 
+    for i in range(L.length):
+        np.random.seed()
+        results = a.search(i, np.array([0]*L.length), np.array([0]*L.length))
+        score = L.heights[np.argmax(results)]
+        scores.append(score)
+    a.score = np.mean(scores)
+
+
+def find_experts(L, t):
+    """Returns agents with top individual search scores"""
+    agents = [] # all possible heuristic profiles
+    for i in range(len(all_perm)):
+        a = Agent(i, all_perm[i], L, sigma)
+        agents.append(a)
+    if __name__ == '__main__':
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            r = [executor.submit(calc_score, a, L) for a in agents]
+    agents.sort(key=lambda x: x.score, reverse=True) # sort agents by expertise
+    return Team(agents[:per_team], L, trust_level=t)
+
+
+def tournament(team, i):
+    np.random.seed()
+    return team.tournament(i)
 
 def run():
     cols = ['smoothness', 'diverse', 'expert', 
             'd_heuristics', 'x_heuristics', 
             'trust', 'sigma', 'poolsize']
     df = pd.DataFrame(columns=cols)
-    h_each = 3 # Number of step sizes each agent has
-    per_team = 6 # Number of agents per team
-    for poolsize in Poolsizes:
-        pool = list(range(1, poolsize+1))
-        all_perm = list(itertools.permutations(pool, r=h_each)) # All possible step size combo
-        for s in Smoothness: # smoothness
-            for sigma in Sigma:
-                for t in Trust: # trust level
-                    np.random.seed()
-                    L = Landscape(s)
-                    """Select set of random starting points"""
-                    starts = random.sample(range(L.length), 100)
-                    """Find experts"""
-                    agents = [] # all possible heuristic profiles
-                    for i in range(len(all_perm)):
-                        agents.append(Agent(i, all_perm[i], L, sigma))
-                    for a in agents:
-                        scores = [] 
-                        for i in starts:
-                            results = a.search(i, np.array([0]*a.landscape.length), np.array([0]*a.landscape.length))
-                            score = L.heights[np.argmax(results)]
-                            scores.append(score)
-                        a.score = np.mean(scores)
-                    agents.sort(key=lambda x: x.score, reverse=True) # sort agents by expertise
-                    expert = Team(agents[:per_team], L, trust_level=t)
-                    
-                    """Create diverse group"""
-                    no_repeat = random.sample(pool, per_team)
-                    repeat = [i for i in pool if i not in no_repeat]
-                    d_heu = [[i]+random.sample(repeat, h_each-1) for i in no_repeat]
-                    
-                    diverse = Team([Agent(i+len(all_perm), d_heu[i], L, sigma) for i in range(per_team)], 
-                                                    L, 
-                                                    trust_level=t)
-            
-                    d_record = []
-                    x_record = []
 
-                    for i in starts:
-                        np.random.seed()
-                        d_record.append(diverse.tournament(i))
-                        x_record.append(expert.tournament(i))
-                   
-                    df = df.append(pd.DataFrame([[s,
-                                                  np.mean(d_record), 
-                                                  np.mean(x_record), 
-                                                  list(itertools.chain.from_iterable([a.h for a in diverse.members])), 
-                                                  list(itertools.chain.from_iterable([a.h for a in expert.members])), 
-                                                  t,
-                                                  sigma,
-                                                  poolsize
-                                                 ]], columns=cols), ignore_index=True)
+    for L in LS:
+        for t in Trust: # trust level
+            
+            expert = find_experts(L, t)
+
+            d_heu = random.sample(all_perm, per_team) # Randomly generate diverse group
+            diverse = Team([Agent(i+len(all_perm), d_heu[i], L, sigma) for i in range(per_team)], 
+                                            L, 
+                                            trust_level=t)
+            d_record = []
+            x_record = []
+
+            if __name__ == '__main__':
+                with concurrent.futures.ThreadPoolExecutor() as executor:
+                    results = [executor.submit(tournament, diverse, i) for i in range(L.length)]
+
+                    for f in concurrent.futures.as_completed(results):
+                        d_record.append(f.result())
+
+                with concurrent.futures.ThreadPoolExecutor() as executor:
+                    results = [executor.submit(tournament, expert, i) for i in range(L.length)]
+
+                    for f in concurrent.futures.as_completed(results):
+                        x_record.append(f.result())
+           
+            df = df.append(pd.DataFrame([[L.s,
+                                          np.mean(d_record), 
+                                          np.mean(x_record), 
+                                          list(itertools.chain.from_iterable([a.h for a in diverse.members])), 
+                                          list(itertools.chain.from_iterable([a.h for a in expert.members])), 
+                                          t,
+                                          sigma,
+                                          poolsize
+                                         ]], columns=cols), ignore_index=True)
     return(df)
 
-# In[86]:
 
-cols = ['smoothness', 'diverse', 'expert', 
-            'd_heuristics', 'x_heuristics', 
-            'trust', 'sigma', 'poolsize']
-data = pd.DataFrame(columns=cols)
-
-all_results = []
-if __name__ == '__main__':
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        results = [executor.submit(run) for i in range(2)]
-
-        for f in concurrent.futures.as_completed(results):
-            all_results.append(f.result())
-
-data = data.append(pd.concat(all_results, ignore_index=True))
-
-data.to_csv('test.csv', index=False)
